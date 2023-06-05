9c5aef84e228ef07854348a13723c060ecd2621a
MrKhatibian <mr.khatibian98@gmail.com>
2023-06-06 00:44:06 +0330

    Use ApplicationDbContext in CategoryController And Make Objecet From it

--- WebMarketApp/Controllers/CategoryController.cs
+++ WebMarketApp/Controllers/CategoryController.cs
@@ -3,11 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMarketApp.Data;
 
 namespace WebMarketApp.Controllers
 {
     public class CategoryController : Controller
     {
+        //Make Object From Baster ApllicationDbContext
+        private readonly ApplicationDbContext _db;
         public IActionResult Index()
         {
             return View();
