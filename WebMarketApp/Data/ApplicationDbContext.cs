1be5aa8447af864aaf48efc2a214a2ed1f4368b0
MrKhatibian <mr.khatibian98@gmail.com>
2023-06-05 21:16:57 +0330

    Add Data and WebMarket Class and install entityFrameWork 5.0.4

---
+++ WebMarketApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMarketApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
