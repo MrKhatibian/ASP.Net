c2a8f6497dfd897671ecdb452c7432a1189ee253
MrKhatibian <mr.khatibian98@gmail.com>
2023-06-05 23:25:18 +0330

    Add DataAnnotations to Category.cs

--- WebMarketApp/Models/Category.cs
+++ WebMarketApp/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace WebMarketApp.Models
 {
     public class Category
     {
+        [Key]
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CreatDateTime { get; set; } = DateTime.Now;
