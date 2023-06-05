857c06ab4aff27b115c4ef2208ec51bddcbc9ca9
MrKhatibian <mr.khatibian98@gmail.com>
2023-06-05 18:19:00 +0330

    Add TagHelper in Category Class for Make Category Table.

--- WebMarketApp/Models/Category.cs
+++ WebMarketApp/Models/Category.cs
@@ -7,5 +7,9 @@ namespace WebMarketApp.Models
 {
     public class Category
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatDateTime { get; set; } = DateTime.Now;
     }
 }
