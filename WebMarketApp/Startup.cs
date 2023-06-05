f1d9aeb49665fd9d1572ebbf0167ff6dd3a20a1e
MrKhatibian <mr.khatibian98@gmail.com>
2023-06-05 22:59:28 +0330

    Add SqlServer Package

--- WebMarketApp/Startup.cs
+++ WebMarketApp/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMarketApp.Data;
 
 namespace WebMarketApp
 {
@@ -24,6 +26,10 @@ namespace WebMarketApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //Add DbContext service
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("MyConnection")
+                ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
