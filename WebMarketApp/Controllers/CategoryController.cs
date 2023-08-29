7a31f9293aaa0b6e1cae7b3287ebabe8e77a8771
MrKhatibian <mr.khatibian98@gmail.com>
2023-08-29 18:47:42 +0330

    Add ModelState for post

--- WebMarketApp/Controllers/CategoryController.cs
+++ WebMarketApp/Controllers/CategoryController.cs
@@ -36,9 +36,13 @@ namespace WebMarketApp.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
         }
     }
 }
