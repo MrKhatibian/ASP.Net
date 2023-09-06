15c96ec795208ebfc2687ce72317685553cdfa93
MrKhatibian <mr.khatibian98@gmail.com>
2023-09-06 16:06:55 +0330

    Add Display Annotation To Name & DisplayOrder

--- WebMarketApp/Models/Category.cs
+++ WebMarketApp/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,8 +12,11 @@ namespace WebMarketApp.Models
         [Key]
         public int ID { get; set; }
         [Required(ErrorMessage ="لطفا نام دسته وارد کنید")]
+        [DisplayName("عنوان دسته")]
         public string Name { get; set; }
         [Required(ErrorMessage = "لطفا ترتیب دسته وارد کنید")]
+        [DisplayName("تزتیب دسته")]
+        
         public int DisplayOrder { get; set; }
         public DateTime CreatDateTime { get; set; } = DateTime.Now;
     }
